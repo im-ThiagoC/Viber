2910d6aabd8ab6a0f056e4f18ef1e101cf1c7105
Im_ThiagoC <thiagomedeiros82013@ufrrj.br>
2025-07-01 15:25:39 -0300

    02: Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+// Header
+// This is the Main Page of the application
+
+// Imports
+import { prisma } from "@/lib/db";
 import { Button } from "@/components/ui/button";
 
-const Page = () => {
+const Page = async () => {
+
   return (
     <div>
       <Button variant="link" className="text-rose-600">
         Click me
       </Button>
     </div>
+
   );
 }
  
