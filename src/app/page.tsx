b4541affe39d8bf35a992d725f5aa7b25af152ae
Im_ThiagoC <thiagomedeiros82013@ufrrj.br>
2025-08-30 14:16:47 -0300

    TC: TRPC Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 // Header
-// This is the Main Page of the application
+// This is the Main Page of the application, this app will be a Lovable Clone, 
+// it makes use of AI to build Websites, it will have a simple UI,
+// Next.js 15, React, Inngest, Prisma
+
+// Use Client Component
+//'use client';
 
 // Imports
-import { prisma } from "@/lib/db";
-import { Button } from "@/components/ui/button";
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query";
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 const Page = async () => {
+  // const trpc = useTRPC();
+
+  // const { data } = useQuery(trpc.createAI.queryOptions({ text: "Thiago!" }));
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Thiago PREFETCH" }));
+  const data = await caller.createAI({ text: "Thiago!" });
 
   return (
-    <div>
-      <Button variant="link" className="text-rose-600">
-        Click me
-      </Button>
-    </div>
-
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
  
