6178b6b724f07630d432341f2a0d9668ee8d4499
Im_ThiagoC <thiagomedeiros82013@ufrrj.br>
2025-08-30 22:34:51 -0300

    TC: AI Jobs!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,17 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 // Imports
 
 const Page = () => {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
@@ -24,7 +28,8 @@ const Page = () => {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ text: "ThiagoC" })}>
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
         Invoke Background Job
       </Button>
     </div>
