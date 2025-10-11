01e4fd006a7ff770ca7123bbd0b3cfb001a648df
Im_ThiagoC <thiagomedeiros82013@ufrrj.br>
2025-10-11 09:51:18 -0300

    TC: Zip Project

--- src/modules/projects/ui/components/message-card.tsx
+++ src/modules/projects/ui/components/message-card.tsx
@@ -66,7 +66,7 @@ const AssistantMessage = ({
 			<div className="flex items-center gap-2 pl-2 mb-2">
 				<Image src="/logo-ai.svg" alt="Viber Logo" width={18} height={18} className="shrink-0"/>
 				<span className="text-sm  font-medium">
-					Viber {fragment?.title}
+					{fragment?.title}
 				</span>
 				<span className="text-sm  text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
 					{format(createdAt, "HH:mm 'on' MMM dd, yyyy")}
